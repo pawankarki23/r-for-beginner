6caaf4506c7a128409a8477c9a35dfc336fa48c8
Pawan Karki <pawankarki5@gmail.com>
2017-08-24 13:18:54 -0400

    added some more lines of codes

--- hello_world.R
+++ hello_world.R
@@ -1,4 +1,15 @@
 help(solve)
+# similar to above
 ?solve
+# vector variable assignment
 x=c(1,2,3)
+# histogram function plot
 hist(lynx)
+source('my_r_code.R') # execute commands stored in an external file
+# getting help about if statement
+help('if')
+# list the objects currently in R session
+objects() # or ls()
+# removing the objects
+rm(x,y,z)
+# currrent objects can be stored in .RData file and command lines in .Rhistory file
